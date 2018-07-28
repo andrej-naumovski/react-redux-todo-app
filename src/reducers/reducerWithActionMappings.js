4f2bc4d5e17c893cecf2d284108168a1e42c3ab6
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-07-29 01:20:39 +0200

    feature/Add empty reducer for todos

---
+++ src/reducers/reducerWithActionMappings.js
@@ -0,0 +1,14 @@
+export const reducerWithActionMappings = (mappings, initialState) => {
+  if (__DEV__ && !initialState) {
+    throw new Error('Initial state must be defined!');
+  }
+
+  return (state, action) => {
+    const reducer = mappings[action.type];
+    if (!reducer) {
+      return state;
+    }
+
+    return reducer(state, action);
+  };
+};
---
+++ src/reducers/todos/todosReducer.js
@@ -0,0 +1,5 @@
+import reducerWithActionMappings from '../reducerWithActionMappings';
+
+const initialTodosState = [];
+
+export default reducerWithActionMappings({}, initialTodosState);
